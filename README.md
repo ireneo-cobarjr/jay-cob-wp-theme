497db522485a275ba71b94580a19c20d67bcd6a1
ireneo-cobarjr <ireneo.cobar.jr@gmail.com>
2019-07-13 22:41:34 +0800

    Adding extra beef to the dev env.

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # jay-cob-wp-theme
-Jay-cob.com Official Theme
+[Jay-cob.com](https://Jay-cob.com) Official WordPress Theme
+
+### Instructions for Noobs
+To build from source, You need to have the following installed on your Computer.
+
+- Node.js
+- WordPress
+- A running local web server like [Valet](https://laravel.com/docs/5.8/valet) or [XAMPP](https://www.apachefriends.org/index.html)
+- Git
+* * *
+If you have all of this already installed:
+1. Open a terminal and navigate to the theme folder of the locally installed WordPress. 
+ `cd localWordPress/wp-content/themes`
+2. Clone this repo by entering this commands on the terminal:
+`git clone https://github.com/ireneo-cobarjr/jay-cob-wp-theme.git`
+`cd jay-cob-wp-theme`
+3. Install dependencies by entering this command on the terminal:
+`npm install`
+4. Open gulpfile.js located on the root directory of jay-cob-wp-theme folder and go to Section B. Change the variable `localsite` to match the local web address of your locally installed WordPress.
+`localsite = 'http://yourWordPress';`
+5. Run the task runner on the terminal:
+`gulp`
+
+
+gulp is using browser-sync. So any changes on files at `src/` will reload the browser.
+* * *
+
+### Libraries Used
+
+- [Bulma](https://bulma.io)
+- [jump.js](http://callmecavs.com/jump.js/)
+- [sal.js](https://mciastek.github.io/sal/)
+- [scene.js](https://daybrush.com/scenejs/)
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
+///////////////////////////////////////////////////////////////////////////
+//Section: A
 //Setting Our Environment Variables
-const {src,dest,watch,parallel,series} = require('gulp');
-const autoprefixer = require('autoprefixer');
-const cleanCss = require('gulp-clean-css');
+const {src,dest,watch,parallel,series,task} = require('gulp');
+const prefix = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
 const concat = require('gulp-concat');
 const lineEnding = require('gulp-line-ending-corrector');
 const rename = require('gulp-rename');
@@ -9,15 +11,68 @@ const gsass = require('gulp-sass');
 const gmaps = require('gulp-sourcemaps');
 const uglify = require('gulp-uglify');
 const babel = require('gulp-babel');
-
-//set variables for file paths
-
-
-
-
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
+const sync =  require('browser-sync');
+///////////////////////////////////////////////////////////////////////////
+//Section: B
+//set variables for files and file paths
+const sass      = 'src/sass/jcob.scss',
+	  cssOut    = 'jcob.min.css',
+	  cssDest   = 'assets/css/',
+	  jsFile    = 'src/js/jcob.js',
+	  jsOut     = 'jcob.min.js', 
+	  jsDest    = 'assets/js/',
+	  localsite = 'http://jay.test';
+///////////////////////////////////////////////////////////////////////////
+//Section: C
+//defining CSS compilation function
+function buildCSS() {
+	return src(sass)
+			.pipe(gmaps.init({ loadMaps: true }))
+			.pipe(gsass().on('error', gsass.logError))
+			.pipe(prefix('last 2 versions'))
+			.pipe(concat(cssOut))
+			.pipe(cleanCSS())
+			.pipe(gmaps.write('./maps/'))
+			.pipe(lineEnding())
+			.pipe(dest(cssDest));
 }
 
-exports.default = defaultTask
+//defining js compilation function
+function buildJS() {
+	return src(jsFile)
+			.pipe(concat(jsOut))
+			.pipe(babel({ presets: ['@babel/env'] }))
+			.pipe(uglify())
+			.pipe(lineEnding())
+			.pipe(dest(jsDest));
+}
+
+//defining file watch function
+function watchFiles() {
+	sync.init({
+		open: 'external',
+		proxy: localsite,
+		port: 8080
+	});	
+
+	//watch for scss file changes
+	watch(sass, buildCSS);
+	//watch for js file changes
+	watch(jsFile, buildJS);
+	//reload browser once changes are made
+	watch([
+		cssDest + cssOut,
+		jsDest  + jsOut 
+		]).on('change', sync.reload);
+}
+
+///////////////////////////////////////////////////////////////////////////
+//Section: D
+exports.watchFiles   = watchFiles;
+exports.buildCSS     = buildCSS;
+exports.buildJS      = buildJS;
+
+var dev = series( parallel([buildJS, buildCSS]), watchFiles);
+task('default', dev);
+///////////////////////////////////////////////////////////////////////////
+
