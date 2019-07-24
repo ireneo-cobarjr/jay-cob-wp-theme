bf34965b59e8b51157786a7a7eb570097119fa4d
ireneo-cobarjr <ireneo.cobar.jr@gmail.com>
2019-07-25 01:32:27 +0800

    Added Services Section

--- template-parts/header/navbar.php
+++ template-parts/header/navbar.php
@@ -2,7 +2,7 @@
   <div class="navbar-brand">
     <a class="navbar-item" href="https://jay-cob.com">
         <?php get_template_part('/template-parts/components/jcWP-logo'); ?>
-      <h3 id="jcWP-banner" class="has-text-dark">Jay Cob</h1>
+      <h4 id="jcWP-banner" class="has-text-dark">Jay Cob</h4>
     </a>
 
     <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
--- template-parts/homepage/homepage-content.php
+++ template-parts/homepage/homepage-content.php
@@ -1,13 +1,13 @@
 <section class="has-background-primary no-overflow">
 	<div class="yellow-section">
 		<div class="container">
-			<div class="columns jcWP-screensize is-vcentered">
+			<div class="columns jcWP-100vh is-vcentered">
 				<div class="column">
-					<div class="has-text-centered">
-						<p>
-							You dont need an alchemist to make "success" from "ideas"
-						</p>
-						<h1>You just need me <br>to build your website</h1>
+					<div class="has-text-centered is-flex" id="homepage-slogan">
+						<div>
+						<p class="has-text-dark">You dont need an alchemist to turn  "ideas" to a "website"</p>
+						<h1>You Only need Me</h1>
+						</div>
 						<div class="buttons are-large is-centered">
 							<button class="button has-background-success has-text-light">Services</button>
 							<button class="button has-background-dark has-text-light">Testimonials</button>
@@ -24,6 +24,29 @@
 </section>
 <section class="has-background-dark">
 	<div class="container">
-		<div class="jcWP-screensize"></div>
+		<div class="columns is-vcentered is-centered jcWP-screensize">
+			<div class="column jcWP-100vh is-8">
+				<div id="service-description-area">
+					<div class="service-description">
+						<h2 class="has-text-light">WordPress Website</h2>
+						<p class="has-text-light">
+							A full Website on WordPress. This already includes a Custom Personlized Theme. Name the pages you need and the sections that are important to you.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="column  jcWP-100vh is-4 has-background-grey-darker">
+				<div class="is-flex" id="services-list">
+					<ul>
+						<li class="has-text-grey-light">WordPress Website</li>
+						<li class="has-text-grey-light">Custom WordPress Theme</li>
+						<li class="has-text-grey-light">Wix to WordPress</li>
+						<li class="has-text-grey-light">Mock-up to HTML</li>
+						<li class="has-text-grey-light">Basic Website</li>
+						<li class="has-text-grey-light">Others</li>
+					</ul>
+				</div>
+			</div>
+		</div>
 	</div>
 </section>
